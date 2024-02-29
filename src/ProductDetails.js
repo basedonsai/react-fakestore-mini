5961a3a2b4558359ccaae0a2eae0d0fd42f8cbe4
Sai Kiran <97734083+basedonsai@users.noreply.github.com>
2024-03-01 04:04:53 +0530

    Update ProductDetails.js

--- src/ProductDetails.js
+++ src/ProductDetails.js
@@ -1,23 +1,23 @@
-// ProductDetails.js
+
 import React from 'react';
 import './ProductDetails.css';
- 
+
 const ProductDetails = ({ product, goBack, addToCart }) => {
   const { image, title, price, description, rating } = product;
 
   return (
     <div className="product-details">
       <button className="back" onClick={goBack}>Back to Products</button>
-      <div style={{ display: 'flex' }}>
-        <img src={image} alt={title} style={{ width: '50%', maxHeight: '65vh' }} />
-        <div style={{ width: '50%', marginLeft: '20px' }}>
+      <div className="product-container">
+        <img src={image} alt={title} />
+        <div className="details-container">
           <h1>{title}</h1>
           <h2>Price: ${price}</h2>
           <h3>Description</h3>
           <p>{description}</p>
           <h3>Rating</h3>
           <p>{rating.rate} ({rating.count} reviews)</p>
-          <button className="back"onClick={() => addToCart(product)}>Add to Cart</button>
+          <button className="add-to-cart" onClick={() => addToCart(product)}>Add to Cart</button>
         </div>
       </div>
     </div>
